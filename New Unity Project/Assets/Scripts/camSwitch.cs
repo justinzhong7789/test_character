764864923248697d0ccc2909cb66a5c424bfe5e5
justinzhong7789 <justinzhong7789@gmail.com>
2020-12-05 19:53:31 -0800

    Camera switch between first and third person perspective now works

---
+++ New Unity Project/Assets/Scripts/camSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSwitch : MonoBehaviour
+{
+    public GameObject firstCam;
+    public GameObject thirdCam;
+    public const int firstMode = 0, thirdMode = 1;
+    public int camMode = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("camera")){
+            if(camMode == firstMode){
+                camMode = thirdMode;
+            }
+            else if(camMode == thirdMode){
+                camMode = firstMode;
+            }
+            StartCoroutine(CameraChange());
+        }
+    }
+
+    IEnumerator CameraChange(){
+        yield return new WaitForSeconds(0.01f);
+        if(camMode == firstMode){
+            firstCam.SetActive(true);
+            thirdCam.SetActive(false);
+        }
+        if(camMode == thirdMode){
+            thirdCam.SetActive(true);
+            firstCam.SetActive(false);
+            
+        }
+    }
+}
